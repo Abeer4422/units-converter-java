f8888687c69f337b45fa2e0ef3d542743fbb4166
unknown <mohammed.f.alyosofi@email.com>
2025-04-28 18:50:49 +0300

    Add temperature unit conversion feature

--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package converter;
 
 import java.util.Scanner;
@@ -11,10 +7,16 @@ public class Converter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Length Unit Converter");
+        System.out.println("Unit Converter");
         System.out.println("1. Meters to Centimeters");
         System.out.println("2. Centimeters to Meters");
-        System.out.print("Choose an option (1 or 2): ");
+        System.out.println("3. Kilograms to Pounds");
+        System.out.println("4. Pounds to Kilograms");
+        System.out.println("5. Celsius to Fahrenheit");
+        System.out.println("6. Fahrenheit to Celsius");
+        System.out.println("7. Celsius to Kelvin");
+        System.out.println("8. Kelvin to Celsius");
+        System.out.print("Choose an option (1, 2, 3 ,4,5 or 7): ");
         int option = scanner.nextInt();
 
         System.out.print("Enter value: ");
@@ -27,9 +29,28 @@ public class Converter {
         } else if (option == 2) {
             result = centimetersToMeters(value);
             System.out.println(value + " centimeters = " + result + " meters");
+        } else if (option == 3) {
+            result = kilogramsToPounds(value);
+            System.out.println(value + " kilograms = " + result + " pounds");
+        } else if (option == 4) {
+            result = poundsToKilograms(value);
+            System.out.println(value + " pounds = " + result + " kilograms");
+        } else if (option == 5) {
+            result = celsiusToFahrenheit(value);
+            System.out.println(value + " Celsius = " + result + " Fahrenheit");
+        } else if (option == 6) {
+            result = fahrenheitToCelsius(value);
+            System.out.println(value + " Fahrenheit = " + result + " Celsius");
+        } else if (option == 7) {
+            result = celsiusToKelvin(value);
+            System.out.println(value + " Celsius = " + result + " Kelvin");
+        } else if (option == 8) {
+            result = kelvinToCelsius(value);
+            System.out.println(value + " Kelvin = " + result + " Celsius");
         } else {
             System.out.println("Invalid option.");
         }
+
     }
 
     public static double metersToCentimeters(double meters) {
@@ -39,5 +60,29 @@ public class Converter {
     public static double centimetersToMeters(double centimeters) {
         return centimeters / 100;
     }
-}
 
+    public static double kilogramsToPounds(double kg) {
+        return kg * 2.20462;
+    }
+
+    public static double poundsToKilograms(double pounds) {
+        return pounds / 2.20462;
+    }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public static double celsiusToKelvin(double celsius) {
+        return celsius + 273.15;
+    }
+
+    public static double kelvinToCelsius(double kelvin) {
+        return kelvin - 273.15;
+    }
+
+}
