bcafbfdb5c7c2256daf14ef9ab794b29eda271b0
Abeer4422 <aalruwaili0044@gmail.com>
2025-04-28 21:56:00 +0300

    Small comment update in main branch to create conflict

--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -11,6 +11,7 @@ public class Converter {
    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+         //update from main branch-----> create conflict .
         System.out.println("Unit Converter");
         System.out.println("1. Meters to Centimeters");
         System.out.println("2. Centimeters to Meters");
